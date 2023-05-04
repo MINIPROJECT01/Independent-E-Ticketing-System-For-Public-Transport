2782737a561e2eb8a28a3f6735ab35e0fe7eea55
MINIPROJECT01 <91179852+MINIPROJECT01@users.noreply.github.com>
2023-05-04 07:44:59 +0530

    Add files via upload

---
+++ NewAddMoney.java
@@ -0,0 +1,14 @@
+package com.example.busticketinguser;
+
+public class NewAddMoney {
+
+    String Money;
+
+    public NewAddMoney(String money) {
+        Money = money;
+    }
+
+    public String getMoney() {
+        return Money;
+    }
+}
